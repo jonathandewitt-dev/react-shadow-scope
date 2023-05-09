e6c27bb9046f90869cc9c971508c912dce98fdf7
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-05-09 19:01:40 -0400

    replacing generated html selectors, instead of hard-coding them -- this should cover the cases where a custom config would change these rules

--- src/tailwind.tsx
+++ src/tailwind.tsx
@@ -48,24 +48,17 @@ export const Tailwind = React.forwardRef<HTMLElement, TailwindProps>(
        	fetch(href)
           .then((response: Response) => response.text())
           .then((tailwind: string) => {
+
+            // since html is out of scope, we have to replace it with :host...
+            const scopedTailwind = tailwind.replace(
+              /(?:^|\s)(html)(?:[^-_a-z])/gi,
+              ':host',
+            );
             const stylesheet = css`
-              ${tailwind}
-              ${`
-                /* apply base styles to host element since html is out of scope */
-                @layer base {
-                  :host {
-                    display: contents;
-                    line-height: 1.5;
-                    -webkit-text-size-adjust: 100%;
-                    -moz-tab-size: 4;
-                    -o-tab-size: 4;
-                      tab-size: 4;
-                    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-                    font-feature-settings: normal;
-                    font-variation-settings: normal;
-                  }
-                }
-              `}
+              :host {
+                display: contents;
+              }
+              ${scopedTailwind}
             `;
             if (adoptedStylesSupported) {
               if (stylesheet instanceof CSSStyleSheet) {
