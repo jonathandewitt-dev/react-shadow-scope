3bd217fed4c81ba0b4a7e6e95f831f3a7f5844e1
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-05-08 23:05:25 -0400

    adding base tailwind styles to :host since the html tag is out of scope

--- src/tailwind.tsx
+++ src/tailwind.tsx
@@ -48,7 +48,25 @@ export const Tailwind = React.forwardRef<HTMLElement, TailwindProps>(
        	fetch(href)
           .then((response: Response) => response.text())
           .then((tailwind: string) => {
-            const stylesheet = css`${tailwind}`;
+            const stylesheet = css`
+              ${tailwind}
+              ${`
+                /* apply base styles to host element since html is out of scope */
+                @layer base {
+                  :host {
+                    display: contents;
+                    line-height: 1.5;
+                    -webkit-text-size-adjust: 100%;
+                    -moz-tab-size: 4;
+                    -o-tab-size: 4;
+                      tab-size: 4;
+                    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+                    font-feature-settings: normal;
+                    font-variation-settings: normal;
+                  }
+                }
+              `}
+            `;
             if (adoptedStylesSupported) {
               if (stylesheet instanceof CSSStyleSheet) {
                 cache.stylesheets.push(stylesheet);
@@ -68,15 +86,13 @@ export const Tailwind = React.forwardRef<HTMLElement, TailwindProps>(
       <react-shadow-scope ref={forwardedRef} {...forwardedProps}>
         <Template shadowrootmode="open" adoptedStyleSheets={cssStyleSheets}>
           <style>
-            {`
-              ${adoptedStylesSupported
-                ? ''
-                : `@import url('${href}');`}
-              :host { display: contents; }
-              ${adoptedStylesSupported
-                ? ''
-                : customStyles}
-            `}
+            {adoptedStylesSupported
+              ? ''
+              : `
+                @import url('${href}');
+                ${customStyles}
+                `
+            }
           </style>
           {children}
         </Template>
